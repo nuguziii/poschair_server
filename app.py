3aebcd17e011a0685aad78c7ac2ef97642616883
Rebecca <rebeccachoosir@gmail.com>
2019-02-20 13:48:09 +0900

    Ver1

--- app.py
+++ app.py
@@ -31,49 +31,40 @@ database = SqliteDatabase(DATABASE)
 class BaseModel(Model):
     class Meta:
         database = database
+        db_table = 'users'
 
 # the user model specifies its fields (or columns) declaratively, like django
 class User(BaseModel):
     name = CharField()
     password = CharField()
-    serialNum = CharField()
+    serialnum = CharField()
     email = CharField(unique=True)
 
 
-# flask provides a "session" object, which allows us to store information across
-# requests (stored by default in a secure cookie).  this function allows us to
-# mark a user as being logged-in by setting some values in the session data:
-def auth_user(user):
-    session['logged_in'] = True
-    session['user_email'] = user.email
-    session['username'] = user.name
-    print('Logged in')
-    #flash('You are logged in as %s' % (user.username))
-
 # get the user from the session
-def get_current_user():
-    if session.get('logged_in'):
-        return User.get(User.email == session['user_email'])
+#def get_current_user(user):
+#    if session.get('logged_in'):
+#        return User.get(User.email == user.email)
 
 # view decorator which indicates that the requesting user must be authenticated
 # before they can access the view.  it checks the session to see if they're
 # logged in, and if not redirects them to the login view.
-def login_required(f):
-    @wraps(f)
-    def inner(*args, **kwargs):
-        if not session.get('logged_in'):
-            return redirect(url_for('login'))
-        return f(*args, **kwargs)
-    return inner
+#def login_required(f):
+#    @wraps(f)
+#    def inner(*args, **kwargs):
+#        if not session.get('logged_in'):
+#            return redirect(url_for('login'))
+#        return f(*args, **kwargs)
+#    return inner
 
 # given a template and a SelectQuery instance, render a paginated list of
 # objects from the query inside the template
-def object_list(template_name, qr, var_name='object_list', **kwargs):
-    kwargs.update(
-        page=int(request.args.get('page', 1)),
-        pages=qr.count() / 20 + 1)
-    kwargs[var_name] = qr.paginate(kwargs['page'])
-    return render_template(template_name, **kwargs)
+#def object_list(template_name, qr, var_name='object_list', **kwargs):
+#    kwargs.update(
+#        page=int(request.args.get('page', 1)),
+#        pages=qr.count() / 20 + 1)
+#    kwargs[var_name] = qr.paginate(kwargs['page'])
+#    return render_template(template_name, **kwargs)
 
 # retrieve a single object matching the specified query or 404 -- this uses the
 # shortcut "get" method on model, which retrieves a single object or raises a
@@ -105,14 +96,15 @@ def homepage():
     # depending on whether the requesting user is logged in or not, show them
     # either the public timeline or their own private timeline
     if request.method == 'POST':
-        if request.form['name']:
+    	userDetails = request.form
+        if len(userDetails) == 4: #join
             try:
                 with database.atomic():
                     # Attempt to create the user. If the username is taken, due to the
                     # unique constraint, the database will raise an IntegrityError.
                     user = User.create(
                         name=request.form['name'],
-                        password=md5((request.form['password']).encode('utf-8')).hexdigest(),
+                        password=request.form['password'],#md5((request.form['password']).encode('utf-8')).hexdigest(),
                         serialnum=request.form['serialnumber'],
                         email=request.form['email'])
 
@@ -123,12 +115,12 @@ def homepage():
             except IntegrityError:
                 return 'already_existed'#flash('That username is already taken')
 
-        elif request.form['email']:
+        elif len(userDetails) == 2: #login
             try:
-                pw_hash = md5(request.form['password'].encode('utf-8')).hexdigest()
+                #pw_hash = md5(request.form['password'].encode('utf-8')).hexdigest()
                 user = User.get(
                     (User.email == request.form['email']) &
-                    (User.password == pw_hash))
+                    (User.password == request.form['password']))
             except User.DoesNotExist:
                 return 'wrong_pw'#flash('The password entered is incorrect')
             else:
