5a6aa1c5d72b097098ac1afe09745e6edd826c5d
Rebecca <rebeccachoosir@gmail.com>
2019-03-26 02:23:10 +0900

    Update server.py

--- Test/server.py
+++ Test/server.py
@@ -1,5 +1,5 @@
 #Server-side script
-from flask import Flask, request
+from flask import Flask, request, g
 import numpy as np
 import torch
 import torch.nn as nn
@@ -7,8 +7,33 @@ import time
 import os
 from data_generator import data
 import json
+from peewee import *
+
+DATABASE = 'POSCHAIR.db'
 
 app = Flask(__name__)
+
+# create a peewee database instance -- our models will use this database to
+# persist information
+database = SqliteDatabase(DATABASE)
+
+# model definitions -- the standard "pattern" is to define a base model class
+# that specifies which database to use.  then, any subclasses will automatically
+# use the correct storage. for more information, see:
+# http://charlesleifer.com/docs/peewee/peewee/models.html#model-api-smells-like-django
+class BaseModel(Model):
+    class Meta:
+        database = database
+        db_table = 'Posture'
+
+# the user model specifies its fields (or columns) declaratively, like django
+class Posture(BaseModel):
+    ID = CharField(unique=True)
+    value = CharField()
+    ultra_value = CharField()
+    label = IntegerField()
+    date = DateTimeField(default=datetime.datetime.now)
+
 
 def test_model(model_name, image):
     #=======================================
@@ -29,22 +54,45 @@ def test_model(model_name, image):
     y_p = y_p.detach().numpy()
     return np.argmax(y_p, axis=1)
 
+@app.before_request
+def before_request():
+    print('before_request')
+    g.db = database
+    g.db.connect()
+
+
+@app.after_request
+def after_request(response):
+    print('close_request')
+    g.db.close()
+    return response
 
 @app.route('/', methods=['POST'])
 def result():
     if request.method=='POST':
-        d = data()
-        
-        data_string = request.form['data']
-        value = json.loads(data_string) # pressure and ultrasonic value from arduino
+        try:
+            d = data()
 
-        value = value[:-2]
-        image = d.generator(value)
-        y = test_model("model0225.pth", image) #model path, image array .. => .. .. ..
-        print("label predicted: ",y)
+            data_string = request.form['data']
+            val = json.loads(data_string) # pressure and ultrasonic value from arduino
 
-        
-        return "received!"
+            with database.atomic():
+                data = Posture.create(
+                    ID="choo@naver.com",
+                    value=val
+                    )
+
+                val = val[:-2]
+                image = d.generator(value)
+                y = test_model("model0225.pth", image) #model path, image array .. => .. .. ..
+                print("label predicted: ",y)
+            return "received!"
+
+        except IntegrityError:
+            return "inserting error"
+
+    else:
+        return "success"
 
 if __name__=='__main__':
  app.run(host='0.0.0.0',port=80,debug=True)
