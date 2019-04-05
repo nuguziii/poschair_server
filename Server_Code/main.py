7f1f955598549ba323608165cfe8ab405253d8d2
Haeyoon Cho <chohy0555@gmail.com>
2019-04-05 20:24:04 +0900

    comment erased

--- Server_Code/main.py
+++ Server_Code/main.py
@@ -57,8 +57,8 @@ def result():
 if __name__ == '__main__':
     global total_pressure
     global total_ultra
-    global num_of_sensor_real_time = 10 # 실시간 (1초마다) 업데이트 되는 이미지
-    global num_of_sensor_total = 10 # n초 동안 받아오는 자세 데이터의 수 #자세 판별시 10개 써서 한다는거
+    global num_of_sensor_real_time = 10
+    global num_of_sensor_total = 10
     global interval = 0.2
     global total_hour = 0
     app.run(host='0.0.0.0', port=80, debug=False)
