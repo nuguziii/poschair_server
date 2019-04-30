b529a01ba313f8509b8d8f7a53803e657637baf3
Rebecca <rebeccachoosir@gmail.com>
2019-04-30 22:26:26 +0900

    Update main_alarm_and_keyword.py

--- Server_Code/main_alarm_and_keyword.py
+++ Server_Code/main_alarm_and_keyword.py
@@ -22,42 +22,42 @@ if __name__ == '__main__':
 
     
     #계속 돌면서 keyword 저장
-    for i in range(2):
+	for i in range(2):
 
         #lower_median_total, upper_median_total DB에서 가져옴
-        c.execute("SELECT lower_median_total FROM Median WHERE ID = ?", ("choo@naver.com",))
-        lower_median_total = c.fetchone()[0]
-        c.execute("SELECT upper_median_total FROM Median WHERE ID = ?", ("choo@naver.com",))
-        upper_median_total = c.fetchone()[0]
+		c.execute("SELECT lower_median_total FROM Median WHERE ID = ?", ("choo@naver.com",))
+		lower_median_total = c.fetchone()[0]
+		c.execute("SELECT upper_median_total FROM Median WHERE ID = ?", ("choo@naver.com",))
+		upper_median_total = c.fetchone()[0]
 
-        lower_median_total = rows.replace('[','').replace(']','').split(',')
-        lower_median_total = list(map(int, lower_median_total))
-        upper_median_total = rows.replace('[','').replace(']','').split(',')
-        upper_median_total = list(map(int, upper_median_total))
+		lower_median_total = rows.replace('[','').replace(']','').split(',')
+		lower_median_total = list(map(int, lower_median_total))
+		upper_median_total = rows.replace('[','').replace(']','').split(',')
+		upper_median_total = list(map(int, upper_median_total))
 
 
-        if np.count_nonzero(np.asarray(lower_median_total)-10)>6: #사용자가 의자에 앉아있는지 판단
+		if np.count_nonzero(np.asarray(lower_median_total)-10)>6: #사용자가 의자에 앉아있는지 판단
             #각 센서값으로 자세 lower/upper 자세 판단 (이건 median 값)
-            lower = LBCNet(d.generator(lower_median_total), d.generator(lower_init)) #lower 자세값
-            upper = upper_balance_check(upper_median_total) #upper 자세값 받아옴.
+			lower = LBCNet(d.generator(lower_median_total), d.generator(lower_init)) #lower 자세값
+			upper = upper_balance_check(upper_median_total) #upper 자세값 받아옴.
 
-            print(lower, upper)
+			print(lower, upper)
 
             
             #실시간 자세 DB에 저장
-            messaging(upper, lower, save_db=True) #output은 int 형태로 나옴 이걸 안드로이드로 전송해서 안드로이드에서 메세지 생성
+			messaging(upper, lower, save_db=True) #output은 int 형태로 나옴 이걸 안드로이드로 전송해서 안드로이드에서 메세지 생성
 			
-    		conn = sqlite3.connect("../../POSCHAIR.db")
-    		c = conn.cursor()
+			conn = sqlite3.connect("../../POSCHAIR.db")
+			c = conn.cursor()
 			input = [datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "choo@naver.com", upper, lower[0], lower[1], lower[2], lower[3]]
-	       	cur.execute("INSERT INTO Posture_data VALUES (?,?,?,?,?,?,?)", input)
+			cur.execute("INSERT INTO Posture_data VALUES (?,?,?,?,?,?,?)", input)
 
             #키워드 매칭 알고리즘(DB에 저장하는 함수)
-            keyword_matching(upper, lower) #자세 값을 기반으로 디비에 해당 키워드 별 +1 해줌
+			keyword_matching(upper, lower) #자세 값을 기반으로 디비에 해당 키워드 별 +1 해줌
 
             #알림 확인 및 전송
-            alarm_list = is_alarm(upper, lower) #알람 보낼 리스트가 있는지 확인
-            print(alarm_list)
-            if len(alarm_list) is not 0: #알람 리스트가 있으면
-                result = generate_alarm(alarm_list) #알람 전송
+			alarm_list = is_alarm(upper, lower) #알람 보낼 리스트가 있는지 확인
+			print(alarm_list)
+			if len(alarm_list) is not 0: #알람 리스트가 있으면
+				result = generate_alarm(alarm_list) #알람 전송
             
