b1e6824e179daeb2624ca963c4839556a6043127
Rebecca <rebeccachoosir@gmail.com>
2019-04-30 22:17:25 +0900

    Update main_alarm_and_keyword.py

--- Server_Code/main_alarm_and_keyword.py
+++ Server_Code/main_alarm_and_keyword.py
@@ -59,7 +59,8 @@ if __name__ == '__main__':
             keyword_matching(upper, lower) #자세 값을 기반으로 디비에 해당 키워드 별 +1 해줌
 
             #알림 확인 및 전송
-            alarm_list = is_alarm() #알람 보낼 리스트가 있는지 확인
+            alarm_list = is_alarm(upper, lower) #알람 보낼 리스트가 있는지 확인
+            print(alarm_list)
             if len(alarm_list) is not 0: #알람 리스트가 있으면
                 result = generate_alarm(alarm_list) #알람 전송
             
