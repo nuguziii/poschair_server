5cd50e46dabea2b01ecb746ff93a385465480e62
heejoe0222 <46959558+heejoe0222@users.noreply.github.com>
2019-05-01 21:22:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
 import datetime
 
 from flask import Flask
-from flask import g
 from flask import redirect
 from flask import request
 from flask import session
 from flask import url_for, abort, render_template, flash
-from functools import wraps
-from hashlib import md5
-
 import sqlite3
 import json
 
 
-# config - aside from our database, the rest is for use by Flask
-DATABASE = 'POSCHAIR.db'
-DEBUG = True
-SECRET_KEY = 'hin6bab8ge25*r=x&amp;+5$0kn=-#log$pt^#@vrqjld!^2ci@g*b'
 
 # create a flask application - this ``app`` object will be used to handle
 # inbound requests, routing them to the proper 'view' functions, etc
@@ -25,47 +17,11 @@ app.config.from_object(__name__)
 
 # create a peewee database instance -- our models will use this database to
 # persist information
-database = SqliteDatabase(DATABASE)
 
 # model definitions -- the standard "pattern" is to define a base model class
 # that specifies which database to use.  then, any subclasses will automatically
 # use the correct storage. for more information, see:
 # http://charlesleifer.com/docs/peewee/peewee/models.html#model-api-smells-like-django
-class BaseModel(Model):
-    class Meta:
-        database = database
-        db_table = 'User'
-
-# the user model specifies its fields (or columns) declaratively, like django
-class User(BaseModel):
-    name = CharField()
-    pwd = CharField()
-    ID = CharField(unique=True)
-    pos_upper = CharField()
-    pos_lower = CharField()
-
-
-def get_object_or_404(model, *expressions):
-    try:
-        return model.get(*expressions)
-    except model.DoesNotExist:
-        abort(404)
-
-
-# Request handlers -- these two hooks are provided by flask and we will use them
-# to create and tear down a database connection on each request.
-@app.before_request
-def before_request():
-    print('before_request')
-    g.db = database
-    g.db.connect()
-
-
-@app.after_request
-def after_request(response):
-	print('close_request')
-	g.db.close()
-	return response
 
 @app.route('/image/',methods=['GET','POST'])
 def getImage():
@@ -76,39 +32,43 @@ def getImage():
 def login():
 	print('login')
 	if request.method == 'POST' and request.form['email']:
-		try:
-			print ('in try')
-			user = User.select(
-                    (User.ID == request.form['email']) &
-                    (User.pwd == request.form['password'])
-                    )
-			print ('get success')
-		except User.DoesNotExist:
-			print ('wrong_pw')
-			return 'wrong_pw'
+            conn = sqlite3.connect("../../POSCHAIR.db")
+            c = conn.cursor()
 
-		else:
-			print ('success')
-			return 'success'
-	return 'success'
+            c.excute("select count(*) from User where ID={}".format(request.form['email']))
+            isUser = c.fetchone()
 
+            if isUser == 1:
+                c.excute("select count(*) from User where ID={} and pwd={}".format(request.form['email'],request.form['password']))
+                isRightPwd = c.fetchone()
+
+                if isRightPwd == 1:
+                    return "success"
+                else:
+                    return "wrong_pw"
+            else:
+                return 'non_email'
 
 @app.route('/signup/', methods=['GET', 'POST'])
 def signup():
 	print('signup')
 	if request.method == 'POST':
-
-		try:
 			print('post')
-			with database.atomic():
-				user = User.create(
-					name=request.form['name'],
-					pwd=request.form['password'],
-                    ID=request.form['email'])
-			return "success"
 
-		except IntegrityError:
-			return 'already_existed'
+            conn = sqlite3.connect("../../POSCHAIR.db")
+            c = conn.cursor()
+
+            c.excute("select count(*) from User where ID={}".format(request.form['email']))
+            isUser = c.fetchone()
+
+            if isUser == 1:
+                return "already_existed"
+            else:
+                c.excute("insert into User(ID,name,pwd) values(?,?,?,?,?)",request.form['email'],request.form['name'],request.form['password'])
+                conn.commit()
+                conn.close()
+
+                return "success"
 
 	return render_template('./index.html')
 
@@ -145,7 +105,7 @@ def sendVideoList():
             rows = c.execute('''
                              select * from Youtube_Video
                              ''').fetchall()
-            conn.commit()
+
             conn.close()
 
             return json.dumps([dict(i) for i in rows])
@@ -186,10 +146,14 @@ def updateVideoLike():
 
 @app.route('/posture/', methods=['GET', 'POST'])
 def getLabel():
+    label=-1
 	#label(int 값) string으로 반환한다
-	if request.method == 'GET':
-
-        return label
+    if request.method == 'GET':
+        if label>7:
+            label=0
+        else:
+            label+=1
+        return str(label)
 
 
 if __name__=='__main__':
