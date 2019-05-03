777eb70bb41f72e39f9371366701145bc0beb451
nuguziii <nuguziii@naver.com>
2019-05-03 18:12:38 +0900

    Update main_alarm_and_keyword.py

--- Server_Code/main_alarm_and_keyword.py
+++ Server_Code/main_alarm_and_keyword.py
@@ -15,11 +15,11 @@ if __name__ == '__main__':
 	c = conn.cursor()
 	c.execute("SELECT init_pos_lower FROM User WHERE ID = ?", ("choo@naver.com",))
 	rows = c.fetchone()[0]
-    
+
 	lower_init = rows.replace('[','').replace(']','').split(',')
 	lower_init = list(map(int, lower_init))
 
-    
+
     #계속 돌면서 keyword 저장
 	for i in range(2):
 
@@ -40,27 +40,24 @@ if __name__ == '__main__':
 			lower = LBCNet(d.generator(lower_median_total), d.generator(lower_init)) #lower 자세값
 			upper = upper_balance_check(upper_median_total) #upper 자세값 받아옴.
 
-			lower = lower.reshape(4).tolist()
 			print(lower, upper)
 
-            
             #실시간 자세 DB에 저장
 			messaging(upper, lower) #output은 int 형태로 나옴 이걸 안드로이드로 전송해서 안드로이드에서 메세지 생성
-			
+
 			conn = sqlite3.connect("../../POSCHAIR.db")
 			c = conn.cursor()
 			input = [datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "choo@naver.com", upper, lower[0], lower[1], lower[2], lower[3]]
 			c.execute("INSERT INTO Posture_data VALUES (?,?,?,?,?,?,?)", input)
 
             #키워드 매칭 알고리즘(DB에 저장하는 함수)
-			keyword_matching(conn, upper, lower) #자세 값을 기반으로 디비에 해당 키워드 별 +1 해줌
+			keyword_matching(conn, upper, lower, lower_median_total) #자세 값을 기반으로 디비에 해당 키워드 별 +1 해줌
 
             #알림 확인 및 전송
-			alarm_list = is_alarm(upper, lower) #알람 보낼 리스트가 있는지 확인
+			alarm_list = is_alarm(upper, lower, conn) #알람 보낼 리스트가 있는지 확인
 			print(alarm_list)
 			try:
 				if len(alarm_list) is not 0: #알람 리스트가 있으면
 					result = generate_alarm(alarm_list) #알람 전송
 			except:
 				pass
-            
