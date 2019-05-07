3df0bc657f13643664857017fa5c3db05e5ae002
Haeyoon Cho <chohy0555@gmail.com>
2019-05-07 19:24:52 +0900

    messaging ported to app.py

--- Server_Code/app.py
+++ Server_Code/app.py
@@ -141,7 +141,31 @@ def updateVideoLike():
 def getLabel():
 	#label(int 값) string으로 반환한다
     if request.method == 'GET':
-        label = random.randrange(0,7) #현재 random인 상태 -> main_messaging 이용해서 수정해야
+        conn = sqlite3.connect("/root/POSCHAIR.db")
+        c = conn.cursor()
+
+        c.execute("SELECT init_pos_lower FROM User WHERE ID = ?", ("choo@naver.com",))
+        lower_origin = c.fetchone()[0]
+        print(lower_origin)
+
+        c.execute("SELECT total_time FROM Keyword WHERE ID = ?", ("choo@naver.com",))
+        total_hour = c.fetchone()[0]
+        print(total_hour)
+
+        '''lower_median DB에서 가져옴'''
+        c.execute("SELECT lower_median FROM Median WHERE ID = ?", ("choo@naver.com",))
+        lower_median = c.fetchone()[0]
+        c.execute("SELECT upper_median FROM Median WHERE ID = ?", ("choo@naver.com",))
+        upper_median = c.fetchone()[0]
+
+        label = 0
+
+        if np.count_nonzero(lower_median-10)>6: #사용자가 의자에 앉아있는지 판단
+            '''각 센서값으로 자세 lower/upper 자세 판단 (이건 median 값)'''
+            lower = LBCNet(d.generator(lower_median), d.generator(lower_origin))
+            upper = upper_balance_check(upper_median) #upper 자세값 받아옴.
+            label = messaging(upper, lower)
+
         return str(label)
 
 
